853078835c3cf1ec769aaac9dd0305ff163d75bf
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-05-07 17:00:47 +0530

    nope i was wrong

--- umeed_website/src/data.js
+++ umeed_website/src/data.js
@@ -16,16 +16,6 @@ export const links = [
         path: "/contact"
     },
 ];
-    //! bug solved 
-        //&export=download (add this at the end of the url to access the file)
-        // https://drive.google.com/drive/folders/1KnzCb5-phmGk6VfVHQQ6WimLHnmPEZVx
-    //TODO intigrate this function to the mainHeaderCarouseldata array
-        // function processGoogleDriveLink(str) {
-        //     if (str.includes("drive.google.com") && str.endsWith("/view?usp=sharing")) {
-        //       return str.replace("/view?usp=sharing", "&export=download");
-        //     }
-        //     return str;
-        //   }
 export const mainHeaderCarouseldata =[
     {
         hashtag: 'MakingIndiaCleaner',
@@ -44,29 +34,5 @@ export const mainHeaderCarouseldata =[
     title: 'Clean Streets, Happy Feet',
     subtext: 'Join us in keeping the streets clean for a brighter future',
     image: 'https://drive.google.com/u/0/uc?id=1-rLiswHURQjk1AjoyE1U0oiTv_RkPAW7&export=download'
-    },
-    {
-    hashtag: 'TrashFreeNation',
-    title: 'From Trash to Treasure',
-    subtext: 'Transforming India into a trash-free nation',
-    image: 'https://drive.google.com/file/d/1ITn0bM1KsTqn4MnxQSBODXEdWjZIH0sV&export=download'
-    },
-    {
-    hashtag: 'CleanRiversProject',
-    title: 'Reviving Indias Rivers',
-    subtext: 'Taking action to clean and restore our precious rivers',
-    image: 'https://drive.google.com/file/d/1UosxpcYnfoERHL6-aHfEV8NVzwkjeZxb&export=download'
-    },
-    {
-    hashtag: 'CleanAirForAll',
-    title: 'Breath of Fresh Air',
-    subtext: 'Working towards cleaner air for every Indian',
-    image: 'https://drive.google.com/file/d/1BSntUVe_h3HTCWNgUxtRszMeKGWYo2cu&export=download'
-    },
-    {
-    hashtag: 'ZeroWasteMovement',
-    title: 'Towards a Zero Waste India',
-    subtext: 'Join the movement to reduce waste and promote recycling',
-    image: 'https://drive.google.com/file/d/1587gDDkakQ1SfF6XePqssTISzrv4d_VU&export=download'
     },
 ]
