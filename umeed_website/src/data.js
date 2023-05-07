075221a007fad727f87c339c487daa9fe3205775
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-05-07 16:56:55 +0530

    solved some bugs with google drive imgaes

--- umeed_website/src/data.js
+++ umeed_website/src/data.js
@@ -16,47 +16,57 @@ export const links = [
         path: "/contact"
     },
 ];
+    //! bug solved 
+        //&export=download (add this at the end of the url to access the file)
+        // https://drive.google.com/drive/folders/1KnzCb5-phmGk6VfVHQQ6WimLHnmPEZVx
+    //TODO intigrate this function to the mainHeaderCarouseldata array
+        // function processGoogleDriveLink(str) {
+        //     if (str.includes("drive.google.com") && str.endsWith("/view?usp=sharing")) {
+        //       return str.replace("/view?usp=sharing", "&export=download");
+        //     }
+        //     return str;
+        //   }
 export const mainHeaderCarouseldata =[
     {
         hashtag: 'MakingIndiaCleaner',
         title: 'Umeed Cleans India',
         subtext: 'A step towards a cleaner India',
-        image: 'https://img.freepik.com/premium-vector/ngo-nongovernmental-organization-serve-specific-social-template-hand-drawn-illustration_2175-7898.jpg?w=2000'
+        image: 'https://drive.google.com/u/0/uc?id=1LqfvQNtcjT_kQN8S3hbme2X3-p99UH8L&export=download'
     },
     {
         hashtag: 'GreenIndiaInitiative',
         title: 'Clean India, Green India',
         subtext: 'Together for a greener and cleaner India',
-        image: 'https://static.vecteezy.com/system/resources/previews/012/919/706/original/ngo-or-non-governmental-organization-to-serve-specific-social-and-political-needs-in-template-hand-drawn-cartoon-flat-illustration-vector.jpg'
+        image: 'https://drive.google.com/u/0/uc?id=1Llp0_OZr5uO4Vgae-nQps1-x9P78Jkrs&export=download'
     },
     {
     hashtag: 'CleanStreetsCampaign',
     title: 'Clean Streets, Happy Feet',
     subtext: 'Join us in keeping the streets clean for a brighter future',
-    image: 'https://img.freepik.com/premium-vector/ngo-nongovernmental-organization-serve-specific-social-template-hand-drawn-illustration_2175-7898.jpg?w=2000'
+    image: 'https://drive.google.com/u/0/uc?id=1-rLiswHURQjk1AjoyE1U0oiTv_RkPAW7&export=download'
     },
     {
     hashtag: 'TrashFreeNation',
     title: 'From Trash to Treasure',
     subtext: 'Transforming India into a trash-free nation',
-    image: 'https://img.freepik.com/premium-vector/ngo-nongovernmental-organization-serve-specific-social-template-hand-drawn-illustration_2175-7898.jpg?w=2000'
+    image: 'https://drive.google.com/file/d/1ITn0bM1KsTqn4MnxQSBODXEdWjZIH0sV&export=download'
     },
     {
     hashtag: 'CleanRiversProject',
     title: 'Reviving Indias Rivers',
     subtext: 'Taking action to clean and restore our precious rivers',
-    image: 'https://static.vecteezy.com/system/resources/previews/012/919/706/original/ngo-or-non-governmental-organization-to-serve-specific-social-and-political-needs-in-template-hand-drawn-cartoon-flat-illustration-vector.jpg'
+    image: 'https://drive.google.com/file/d/1UosxpcYnfoERHL6-aHfEV8NVzwkjeZxb&export=download'
     },
     {
     hashtag: 'CleanAirForAll',
     title: 'Breath of Fresh Air',
     subtext: 'Working towards cleaner air for every Indian',
-    image: 'https://img.freepik.com/premium-vector/ngo-nongovernmental-organization-serve-specific-social-template-hand-drawn-illustration_2175-7898.jpg?w=2000'
+    image: 'https://drive.google.com/file/d/1BSntUVe_h3HTCWNgUxtRszMeKGWYo2cu&export=download'
     },
     {
     hashtag: 'ZeroWasteMovement',
     title: 'Towards a Zero Waste India',
     subtext: 'Join the movement to reduce waste and promote recycling',
-    image: 'https://static.vecteezy.com/system/resources/previews/012/919/706/original/ngo-or-non-governmental-organization-to-serve-specific-social-and-political-needs-in-template-hand-drawn-cartoon-flat-illustration-vector.jpg'
+    image: 'https://drive.google.com/file/d/1587gDDkakQ1SfF6XePqssTISzrv4d_VU&export=download'
     },
 ]
